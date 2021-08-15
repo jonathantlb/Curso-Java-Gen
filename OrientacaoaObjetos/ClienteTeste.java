c704fbd668a20dddcabae3dd527587ab1478bd16
Jonathan Tadeu Leite Bonfim <jonathan_tlb@hotmail.com>
2021-08-15 15:53:42 -0300

    adição de exercicios Orientação a Objetos

---
+++ OrientacaoaObjetos/ClienteTeste.java
@@ -0,0 +1,14 @@
+package  OrientacaoaObjetos ;
+
+public  class  ClienteTeste {
+
+	public  static  void  main ( String [] args ) {
+		// TODO stub do método gerado automaticamente
+
+		Cliente c1 =  new  Cliente ( 111 , " Joselito " , " Semnocao " , " joselitosn@hotmail.com " , false );
+		
+	
+		c1 . fichaCliente ();
+	}
+
+}
